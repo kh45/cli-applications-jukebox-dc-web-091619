3d19264a24851135daefd8ae0c2eb77560670dca
industrious-modal-4420 <khaledhassan45@gmail.com>
2019-09-07 19:58:13 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -21,7 +21,7 @@ end
 def play(songs)
  puts "Please enter a song name or number:"
  choice = gets.strip
- if ssongs.include?(choice)
+ if songs.include?(choice)
   puts "Playing #{choice}"
  elsif choice.to_i > 0 && choice.to_i < 10
   puts "Playing #{songs[choice.to_i - 1]}"
